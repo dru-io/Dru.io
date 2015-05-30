7adaf9bd041608840532a3d0227c77e1ef7009b4
Alan Bondarchuk <imacoda@gmail.com>
2015-05-30 17:58:36 +0500

    move search form into $page array

--- sites/all/themes/druiot/template.php
+++ sites/all/themes/druiot/template.php
@@ -44,9 +44,11 @@ function druiot_preprocess_page(&$variables) {
   //  $variables['header_links'] = theme('druiot_header_links');
 
   // Search form.
-  $variables['header_search_form'] = '<form action="/search" id="site-search">
-          <input name="s" value="" maxlength="128" class="form-search" type="text" placeholder="Поиск по сообществу">
-        </form>';
+  $variables['page']['header_search_form'] = array(
+    '#markup' => '<form action="/search" id="site-search">
+                    <input name="s" value="" maxlength="128" class="form-search" type="text" placeholder="Поиск по сообществу">
+                  </form>'
+  );
 
   // Header profile data.
   $variables['page']['header_profile'] = array(
