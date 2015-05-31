41680264b2b0ca69832e213367c5b2fdf2e50183
Alan Bondarchuk <imacoda@gmail.com>
2015-05-31 20:30:18 +0500

    acebueditor update code

--- sites/all/modules/custom/acebueditor/js/ace.bueditor.js
+++ sites/all/modules/custom/acebueditor/js/ace.bueditor.js
@@ -1,8 +1,35 @@
 var BUE = window.BUE = window.BUE || {preset: {}, templates: {}, instances: [], preprocess: {}, postprocess: {}};
+
 BUE.postprocess.acebueditor = function (E, $) {
-  E.showAceEditorDialog = function () {
+  var ace_src = Drupal.settings.basePath + Drupal.settings.ace_editor.ace_src;
+  ace.config.set('basePath', ace_src);
+
+  E.showAcedCodeDialog = function () {
     var title = Drupal.t('Insert code');
-    var content = $('<textarea class="aceeditor-textarea"></textarea>');
-    BUE.dialog.open(title, content, 'fadeIn');
+    var $form = $('<form class="ace-editor-wrapper"><div id="ace-editor"></div></form>');
+    var $button = $('<input type="button" id="ace-insert" class="form-submit" value="' + Drupal.t('OK') + '" />');
+    $button.click(function() {
+      var insert_text = editor_session.getValue().replace("<?php\n\n", '');
+      insert_text = "~~~php\n" + insert_text + "\n~~~";
+      E.replaceSelection(insert_text);
+      E.dialog.close();
+    });
+    $form.append($button);
+    E.dialog.open(title, $form, 'fadeIn');
+
+    var editor = ace.edit('ace-editor');
+    var editor_session = editor.session;
+    var initial_value = E.getSelection();
+    if(initial_value.indexOf('<?php') == -1) {
+      initial_value = "<?php\n\n" + initial_value;
+    }
+    editor.setValue(initial_value);
+    editor.gotoLine(editor.session.getLength() + 1);
+    editor.setOptions({
+      minLines: 10,
+      maxLines: 20,
+      theme: 'ace/theme/twilight',
+      mode: 'ace/mode/php'
+    });
   };
 };
