918a81d9cbde3db91aa5a693dfa85e1821b74436
Alan Bondarchuk <imacoda@gmail.com>
2015-05-31 23:53:48 +0500

    hybridauth module update to dev for vk provider

--- sites/all/modules/contrib/hybridauth/js/hybridauth.admin.js
+++ sites/all/modules/contrib/hybridauth/js/hybridauth.admin.js
@@ -155,6 +155,14 @@
       else {
         vals.push(label + ': ' + Drupal.t('return to the same page'));
       }
+      value = $('#edit-hybridauth-destination-error', context).attr('value');
+      label = '<span style="font-weight:bold;">' + $.trim($('label[for="edit-hybridauth-destination-error"]', context).text()) + '</span>';
+      if (value) {
+        vals.push(label + ': ' + value);
+      }
+      else {
+        vals.push(label + ': ' + Drupal.t('return to the same page'));
+      }
 
       label = '<span style="font-weight:bold;">' + $.trim($('label[for="edit-hybridauth-duplicate-emails"]', context).text()) + '</span>';
       var list = [];
