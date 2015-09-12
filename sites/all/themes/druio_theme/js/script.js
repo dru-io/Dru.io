fee779bcf2b5b41cd8ea2f8829dc1b5d209ef9d0
adubovskoy <dan@ra-don.ru>
2015-09-12 20:00:31 +0300

    Rate template

--- sites/all/themes/druio_theme/js/script.js
+++ sites/all/themes/druio_theme/js/script.js
@@ -7,7 +7,15 @@
 
   Drupal.behaviors.druio_theme = {
     attach: function (context, settings) {
+      // Fix wrapper for Rate module.
+      $('.rate-widget').each(function () {
+        $(this).addClass($(this).attr('id'));
+      });
 
+      $('#make-reply').click(function() {
+        $(this).hide();
+        $('form.node-answer-form').fadeIn();
+      });
     }
   };
 
