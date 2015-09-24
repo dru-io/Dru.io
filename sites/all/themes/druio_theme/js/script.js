019a9725a2ec705b65e8f3bcdf1b1fb61cd24ac6
Niklan <niklanrus@gmail.com>
2015-09-24 21:33:06 +0300

    Added MD sidebar and header.

--- sites/all/themes/druio_theme/js/script.js
+++ sites/all/themes/druio_theme/js/script.js
@@ -8,18 +8,63 @@
   Drupal.behaviors.druio_theme = {
     attach: function (context, settings) {
       // Attach highlightjs.
-      $('pre code').each(function (i, block) {
-        hljs.highlightBlock(block);
-      });
+      if ($('pre code').length) {
+        $('pre code').each(function (i, block) {
+          hljs.highlightBlock(block);
+        });
+      }
 
       // Fix wrapper for Rate module.
-      $('.rate-widget').each(function () {
-        $(this).addClass($(this).attr('id'));
-      });
+      if ($('.rate-widget').length) {
+        $('.rate-widget').each(function () {
+          $(this).addClass($(this).attr('id'));
+        });
+      }
 
-      $('#make-reply').click(function() {
-        $(this).hide();
-        $('form.node-answer-form').fadeIn();
+      if ($('#make-reply').length) {
+        $('#make-reply').click(function () {
+          $(this).hide();
+          $('form.node-answer-form').fadeIn();
+        });
+      }
+
+      /**
+       * Sidebar for tablet size media queries.
+       */
+      // Button for toggle sidebar.
+      var sidebarButton = $('<a href="#" id="sidebar-toggle">Сайдбар</a>');
+      // Function which did all work.
+      function tablet_sidebar() {
+        var width = $(window).width(),
+        // This is our tablet media query sizes.
+          minWidth = 768,
+          maxWidth = 992;
+
+        // If we in media query breakpoint.
+        if (width >= minWidth && width <= maxWidth) {
+          $('#main', context).prepend(sidebarButton);
+          // Attach click listener to our button.
+          sidebarButton.on('click', function (e) {
+            $('body', context).toggleClass('sidebar-open');
+            // Set min-height for content equals of sidebar height.
+            var contentHeight = $('#content', context).height(),
+              sidebarHeight = $('#sidebar', context).height();
+            if (contentHeight < sidebarHeight) {
+              $('#content', context).css('min-height', sidebarHeight);
+            }
+            e.preventDefault();
+          });
+        }
+        // If we outside this sizes.
+        else {
+          sidebarButton.remove();
+        }
+      }
+      // First at all we call this function on page load.
+      tablet_sidebar();
+      // Attach event on widnow resize.
+      $(window).on('resize', function (e) {
+        tablet_sidebar();
       });
     }
   };
