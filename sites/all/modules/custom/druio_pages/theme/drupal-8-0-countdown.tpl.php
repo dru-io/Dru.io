2a234e13e516a371d6db680621e04885c61f0f65
Niklan <niklanrus@gmail.com>
2015-11-13 21:32:22 +0500

    - Added countdown template.

---
+++ sites/all/modules/custom/druio_pages/theme/drupal-8-0-countdown.tpl.php
@@ -0,0 +1,8 @@
+<section id="drupal-8-countdown">
+  <h2>Drupal 8 уже скоро!</h2>
+  <p>Drupal 8 - это 5 лет разработки, скорость и гибкость. Создан с использованием ООП и впитал в себя лучшие наработки сторонних проектов Open Source, таких как Symfony, Twig, Composer, Backbone и многие другие.</p>
+  <div class="buttons">
+    <a href="http://ply.st/drupal/8.x-1.x" target="_blank" rel="nofollow" class="demo">Попробовать</a>
+    <a href="/drupal-8.0" class="more">Узнать больше</a>
+  </div>
+</section>
