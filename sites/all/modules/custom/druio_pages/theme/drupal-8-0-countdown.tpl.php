3d644ceaf4ca70046e63b61d79c607084e8ee0ac
Niklan <niklanrus@gmail.com>
2017-07-30 14:23:19 +0000

    Frontpage text

--- sites/all/modules/custom/druio_pages/theme/drupal-8-0-countdown.tpl.php
+++ sites/all/modules/custom/druio_pages/theme/drupal-8-0-countdown.tpl.php
@@ -1,11 +1,11 @@
 <section id="drupal-8-countdown">
   <div class="content">
-    <h2>Drupal 8 уже здесь!</h2>
+    <h2>Что такое Drupal?</h2>
     <h3>Drupal = Сообщество + Ты</h3>
-    <p>Drupal 8 - это 5 лет разработки, более 3800 контрибьюторов, это совершенно новый уровень. Создан с использованием ООП и впитал в себя лучшие наработки сторонних проектов Open Source, таких как Symfony, Twig, Composer, Backbone и многие другие.</p>
+    <p>Drupal это более 30 000 контрибьюторов, это совершенно новый уровень разработки. Создан с использованием ООП и впитал в себя лучшие наработки сторонних проектов Open Source, таких как Symfony, Twig, Composer и многие другие.</p>
     <div class="buttons">
-      <a href="http://ply.st/drupal/8.x-1.x" target="_blank" rel="nofollow" class="demo">Попробовать</a>
-      <a href="/drupal-8.0" class="more">Узнать больше</a>
+      <a href="http://ply.st/drupal/" target="_blank" rel="nofollow" class="demo">Попробовать онлайн</a>
+      <a href="http://drupal.dru.io" target="_blank" class="more">Подробнее о Drupal</a>
     </div>
   </div>
 </section>
