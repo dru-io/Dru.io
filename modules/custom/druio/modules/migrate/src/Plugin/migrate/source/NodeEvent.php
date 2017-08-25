4d3cc0cf8af1bc47a6ffa7fbfe7aafdf134b39ca
Niklan <niklanrus@gmail.com>
2017-08-25 19:54:26 +0500

    Migration for events is now fully completed. 🎉

--- modules/custom/druio/modules/migrate/src/Plugin/migrate/source/NodeEvent.php
+++ modules/custom/druio/modules/migrate/src/Plugin/migrate/source/NodeEvent.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\druio_migrate\Plugin\migrate\source;
 
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
 use Drupal\migrate\Row;
 
@@ -49,6 +50,9 @@ class NodeEvent extends SqlBase {
       'body_value' => $this->t('The value of body field'),
       'body_format' => $this->t('The format of body field'),
       'field_event_poster_fid' => $this->t('File ID for poster image'),
+      'field_data_field_event_date' => $this->t('The date of the event'),
+      'field_data_field_event_place' => $this->t('The place of the event'),
+      'city_tid' => $this->t('City TID in the current database'),
     ];
 
     return $fields;
@@ -84,14 +88,62 @@ class NodeEvent extends SqlBase {
     $row->setSourceProperty('body_format', $body_query['body_format']);
 
     // Poster FID.
-    $body_query = $this->select('field_data_field_event_poster', 'p')
+    $poster_query = $this->select('field_data_field_event_poster', 'p')
       ->fields('p', ['field_event_poster_fid'])
       ->condition('entity_type', 'node')
       ->condition('bundle', 'event')
       ->condition('entity_id', $nid)
       ->execute()
       ->fetch();
-    $row->setSourceProperty('field_event_poster_fid', $body_query['field_event_poster_fid']);
+    $row->setSourceProperty('field_event_poster_fid', $poster_query['field_event_poster_fid']);
+
+    // Event date.
+    $event_date_query = $this->select('field_data_field_event_date', 'd')
+      ->fields('d', ['field_event_date_value'])
+      ->condition('entity_type', 'node')
+      ->condition('bundle', 'event')
+      ->condition('entity_id', $nid)
+      ->execute()
+      ->fetch();
+    $date = new DrupalDateTime($event_date_query['field_event_date_value']);
+    $date->setTimezone(new \DateTimezone(DATETIME_STORAGE_TIMEZONE));
+    $formatted = $date->format(DATETIME_DATETIME_STORAGE_FORMAT);
+    $row->setSourceProperty('field_event_date_value', $formatted);
+
+    // Event place.
+    $event_place_query = $this->select('field_data_field_event_place', 'p')
+      ->fields('p', ['field_event_place_value'])
+      ->condition('entity_type', 'node')
+      ->condition('bundle', 'event')
+      ->condition('entity_id', $nid)
+      ->execute()
+      ->fetch();
+    $row->setSourceProperty('field_event_place_value', $event_place_query['field_event_place_value']);
+
+    // City tid. The main thing here, is to find TID in current database for
+    // city, based on city name. This is done because cities is not migrated
+    // from the old site, the are imported from new CSV file which has nothing
+    // to do with old structure. So we need to handle this manually.
+    // This Query builder is not supported for joins, so we split them.
+    $old_city_tid = $this->select('field_data_field_event_city', 'c')
+      ->fields('c', ['field_event_city_tid'])
+      ->condition('entity_type', 'node')
+      ->condition('bundle', 'event')
+      ->condition('entity_id', $nid)
+      ->execute()
+      ->fetch();
+    $old_city_name = $this->select('taxonomy_term_data', 'n')
+      ->fields('n', ['name'])
+      ->condition('tid', $old_city_tid['field_event_city_tid'])
+      ->execute()
+      ->fetch();
+    $current_city_tid = \Drupal::database()->select('taxonomy_term_field_data', 'td')
+      ->fields('td', ['tid'])
+      ->condition('name', $old_city_name['name'], 'LIKE')
+      ->execute()
+      ->fetch();
+    $row->setSourceProperty('city_tid', $current_city_tid->tid);
+
     return parent::prepareRow($row);
   }
 
