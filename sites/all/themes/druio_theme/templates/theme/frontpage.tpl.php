5bf0c2cc414afd875ab75ec2edbbad75de21b1c3
Niklan <niklanrus@gmail.com>
2017-12-25 13:08:14 +0300

    Revert "Added DrupalCamp Kransodar 2017 banner"
    This reverts commit 7e086ab5b30a58cc7460f06f9a7af753aa3e37d2.

--- sites/all/themes/druio_theme/templates/theme/frontpage.tpl.php
+++ sites/all/themes/druio_theme/templates/theme/frontpage.tpl.php
@@ -20,38 +20,6 @@
   <?php print views_embed_view('promoted_content', 'block'); ?>
 </section>
 
-<section id="drupalcamp-ug-2017">
-  <div class="camp-pane">
-    <div class="logo-wrapper">
-      <img src="/sites/default/files/custom/drupal-ug/yug-logo-bigg.png" alt="DrupalCamp Краснодар логотип." class="logo">
-    </div>
-
-    <div class="content-wrapper">
-      <div class="title">DrupalCamp Краснодар 2017</div>
-
-      <div class="description">
-        <p>DrupalCamp Краснодар 2017 — значимое событие для российского Друпал-сообщества — это выражение нашей приверженности к сотрудничеству и взаимному обогащению знаниями и опытом. Мы привлечем множество интересных людей, которые наполнят Краснодар энергией конструктивного общения, познавательными кейсами и конечно же Друпалом.</p>
-
-        <p>DrupalCamp — важная часть жизни Друпал-сообщества, способствующая его сплочению, привлекающая внимание IT-сообщества к Друпалу и общества в целом к разработке приложений и сайтов, а также к смежным сферам.</p>
-
-        <p>Разработчики, веб-мастера, дизайнеры, менеджеры проектов, владельцы бизнеса и работодатели — внимание большого количества людей будет привлечено нашим событием.</p>
-
-        <p>Интересно будет всем, ждем вас 16 декабря в Краснодаре!</p>
-      </div>
-
-      <div class="buttons">
-        <a href="http://2017.drupalyug.ru/?utm_source=druio&utm_medium=banner&utm_campaign=frontpage" class="website" target="_blank">Официальный сайт конференции</a>
-        <span class="youtube-button">Видео приглашение</span>
-      </div>
-    </div>
-  </div>
-
-  <div class="video-pane">
-    <div class="close"></div>
-    <div class="video"></div>
-  </div>
-</section>
-
 <section id="frontpage-latest">
   <?php print views_embed_view('question_nodes', 'frontpage'); ?>
   <?php print views_embed_view('orders', 'frontpage'); ?>
