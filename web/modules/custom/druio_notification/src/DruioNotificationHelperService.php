81c1b50f54a0b9a440aa66604627b41ca07259c1
Niklan <niklanrus@gmail.com>
2018-01-22 18:45:30 +0500

    - Added new method getUserNotifications() for druio_notification.helper service. - Added new method getUserUnreadCount() for druio_notification.helper service. - Added two constants to DruioNnotificationInterface.

--- web/modules/custom/druio_notification/src/DruioNotificationHelperService.php
+++ web/modules/custom/druio_notification/src/DruioNotificationHelperService.php
@@ -3,6 +3,7 @@
 namespace Drupal\druio_notification;
 
 use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\druio_notification\Entity\DruioNotificationInterface;
 
 /**
  * Class DruioNotificationHelperService.
@@ -19,6 +20,39 @@ class DruioNotificationHelperService {
    */
   public function __construct(EntityTypeManager $entityTypeManager) {
     $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Returns user notifications.
+   *
+   * @param int $uid
+   *   User ID for which need to load notifications.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   Notification entities if found, NULL if not.
+   */
+  public function getUserNotifications($uid) {
+    return $this->entityTypeManager->getStorage('druio_notification')
+      ->loadByProperties([
+        'user_id' => $uid,
+      ]);
+  }
+
+  /**
+   * Returns count of unread notifications for specific user.
+   *
+   * @param int $uid
+   *   User ID for which need to get count of unread notifications.
+   *
+   * @return int
+   *   The number of unread notifications.
+   */
+  public function getUserUnreadCount($uid) {
+    $query = $this->entityTypeManager->getStorage('druio_notification')
+      ->getQuery()
+      ->condition('user_id', $uid)
+      ->condition('is_read', DruioNotificationInterface::NOT_READ);
+    return $query->count()->execute();
   }
 
 }
--- web/modules/custom/druio_notification/src/Entity/DruioNotificationInterface.php
+++ web/modules/custom/druio_notification/src/Entity/DruioNotificationInterface.php
@@ -14,6 +14,16 @@ use Drupal\user\EntityOwnerInterface;
 interface DruioNotificationInterface extends ContentEntityInterface, EntityChangedInterface, EntityOwnerInterface {
 
   /**
+   * Denotes that notification was read by user.
+   */
+  const READ = TRUE;
+
+  /**
+   * Denotes that notification wasn't read by user.
+   */
+  const NOT_READ = FALSE;
+
+  /**
    * Mark notification as read.
    */
   public function markAsRead();
