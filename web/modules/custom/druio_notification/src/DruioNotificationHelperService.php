05d75793cc769033b4c637c1c19a926126b2bc24
Niklan <niklanrus@gmail.com>
2018-01-22 18:31:47 +0500

    Added entityTypeManager as dependency for druio_notification.service.

--- web/modules/custom/druio_notification/src/DruioNotificationHelperService.php
+++ web/modules/custom/druio_notification/src/DruioNotificationHelperService.php
@@ -2,16 +2,23 @@
 
 namespace Drupal\druio_notification;
 
+use Drupal\Core\Entity\EntityTypeManager;
+
 /**
  * Class DruioNotificationHelperService.
  */
 class DruioNotificationHelperService {
 
   /**
+   * @var EntityTypeManager.
+   */
+  protected $entityTypeManager;
+
+  /**
    * Constructs a new DruioNotificationHelperService object.
    */
-  public function __construct() {
-
+  public function __construct(EntityTypeManager $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
   }
 
 }
