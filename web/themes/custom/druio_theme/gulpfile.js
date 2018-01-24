fbaca15a1c85f3dd568b0f06b99c1f79ead7695a
Niklan <niklanrus@gmail.com>
2018-01-24 19:54:03 +0500

    A little bit changed gulpfile.

--- web/themes/custom/druio_theme/gulpfile.js
+++ web/themes/custom/druio_theme/gulpfile.js
@@ -7,18 +7,19 @@ const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./assets/scss/styles.scss')
-    .pipe(sourcemaps.init())
+    // Something wronng with sourcemap. Temporary disabled.
+    //.pipe(sourcemaps.init())
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('./maps'))
+    //.pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./assets/css/'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./assets/scss/**.scss', ['sass']);
+  gulp.watch('./assets/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['watch', 'sass']);
