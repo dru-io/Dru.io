f0e4b3bcdf8d577f42f80ec6525bcd808287f737
Niklan <niklanrus@gmail.com>
2018-02-05 19:02:42 +0500

    - Added new service druio_question. - Added method getQuestionAnswerCount() for druio_question service.

---
+++ web/modules/custom/druio_question/src/DruioQuestionService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\druio_question;
+
+use Drupal\Core\Entity\EntityTypeManager;
+
+/**
+ * Class DruioQuestionService.
+ */
+class DruioQuestionService {
+
+  /**
+   * @var EntityTypeManager.
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new DruioQuestionService object.
+   */
+  public function __construct(EntityTypeManager $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Returns how much question has answers.
+   *
+   * @param int $qid
+   *   Question node ID for which answer count must be returned.
+   *
+   * @return int
+   *   The answers count for question.
+   */
+  public function getQuestionAnswerCount($qid) {
+    $query = $this->entityTypeManager->getStorage('node')
+      ->condition('type', 'question');
+    return $query->count()->execute();
+  }
+
+}
