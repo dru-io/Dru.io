2c2b6c6b9821cf06c0ce12c1a1dbee7661f9ce4e
Niklan <niklanrus@gmail.com>
2018-02-07 20:18:24 +0500

    Improved and finished relative time JS.

--- web/themes/custom/druio_theme/assets/js/created-relative-time.js
+++ web/themes/custom/druio_theme/assets/js/created-relative-time.js
@@ -37,25 +37,49 @@
       let timeCreated = DateTime.fromMillis(timestamp * 1000);
       let different = timeCurrent.diff(timeCreated, ['days', 'hours', 'minutes', 'seconds']);
       let result = '';
-      // Luxon doesn't suppoer relative time for now, so we handle it manually.
+      let translations = {
+        'days': Drupal.formatPlural(different.days, '1 day', '@count days', {}, { context: 'Relative time' }),
+        'hours': Drupal.formatPlural(different.hours, '1 hour', '@count hours', {}, { context: 'Relative time' }),
+        'minutes': Drupal.formatPlural(different.minutes, '1 minute', '@count minutes', {}, { context: 'Relative time' }),
+        // Seconds are decimals in Luxon. We parse only int value.
+        'seconds': Drupal.formatPlural(parseInt(different.seconds), '1 second', '@count seconds', {}, { context: 'Relative time' }),
+      };
+
+      // Luxon doesn't support relative time for now, so we handle it manually.
       // @see https://github.com/moment/luxon/blob/40bfc1396d06fd8f7996843dbc913db29428329d/docs/moment.md#major-functional-differences
-      // @todo find out why it fails on second plural form. Issue #2934480.
       if (different.days > 0) {
-        result = Drupal.formatPlural(different.days, '@count day', `@count days`);
+        result = this.prepareTranslation(different.hours, translations.days, translations.hours);
       }
       else if (different.hours > 0) {
-        result = Drupal.formatPlural(different.hours, '@count hour', `@count hours`);
+        result = this.prepareTranslation(different.minutes, translations.hours, translations.minutes);
       }
       else if (different.minutes > 0) {
-        result = Drupal.formatPlural(different.hours, '@count minute', `@count minutes`);
+        result = this.prepareTranslation(parseInt(different.seconds), translations.minutes, translations.seconds);
       }
       else {
-        // Seconds are decimals in Luxon. We parse only int value.
-        result = Drupal.formatPlural(parseInt(different.seconds), '@count second', `@count seconds`);
+        result = this.prepareTranslation(0, translations.seconds);
       }
 
       $(element).html(result);
     },
+
+    /**
+     * Prepare translated string.
+     */
+    prepareTranslation: function(secondValue, firstString, secondString = '') {
+      if (secondValue === 0) {
+        return Drupal.t('@value ago', {
+          '@value': firstString,
+        }, { context: 'Relative time' });
+      }
+      else {
+        return Drupal.t('@first and @second ago', {
+          '@first': firstString,
+          '@second': secondString,
+        }, { context: 'Relative time' });
+      }
+
+    }
   };
 
 })(jQuery, Drupal, luxon.DateTime);
